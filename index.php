2eeb1471a16e84fa38e2b30204fc3524192cf1cc
HENRIQUES-SILVA Simao <Simao.HENRIQUES-SILVA@cpnv.ch>
2023-02-10 10:58:06 +0100

    feat:snows

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 
 session_start();
+require "controller/SnowsController.php";
 require "controller/navigation.php";
+
 
 if (isset($_GET['action'])) {
   $action = $_GET['action'];
   switch ($action) {
       case 'home' :
           home();
+          break;
+      case 'snows' :
+          snows();
           break;
       default :
           lost();
---
+++ view/snows.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pascal.BENZONANA
+ * Date: 08.05.2017
+ * Time: 09:16
+ */
+
+// tampon de flux stocké en mémoire
+ob_start();
+$title="RentASnow - Snows";
+?>
+
+    <div class="span12" id="divMain">
+        <h1>Vous aurez ici des snowboards !</h1>
+
+        <a href="index.php?action=home">Retourner à l'accueil</a>
+
+
+<?php
+$content = ob_get_clean();
+require "gabarit.php";
